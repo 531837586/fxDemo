12c0a61a01a0d46b04efdda22574905acf6d64f2
樊星 <fanxing@fanxingdeMacBook-Pro.local>
2019-03-06 09:50:46 +0800

    Initial Commit

---
+++ fxDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fxDemo
+//
+//  Created by 樊星 on 2019/3/6.
+//  Copyright © 2019 樊星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fxDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  fxDemo
+//
+//  Created by 樊星 on 2019/3/6.
+//  Copyright © 2019 樊星. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
